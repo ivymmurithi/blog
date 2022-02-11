7e9737db9bc8aedd825f8ec076b53313da82b616
ivymmurithi <ivymurithi@gmail.com>
2022-02-11 13:57:41 +0300

    add main file to run app and views

---
+++ main.py
@@ -0,0 +1,34 @@
+from db import  app, db
+from flask import render_template
+from config import *
+from flask_script import Manager, Server
+from flask_migrate import Migrate,MigrateCommand
+
+manager = Manager(app)
+migrate = Migrate(app,db)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+manager.add_command('server',Server)
+manager.add_command('db',MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app,db=db)
+
+if __name__ == '__main__':
+    manager.run()
