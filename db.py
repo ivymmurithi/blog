5a5f9d4b7bf1bd6b963387a7863498a67ed8b2e4
ivymmurithi <ivymurithi@gmail.com>
2022-02-11 13:58:14 +0300

    add db file for sqlalchemy and development configurations

---
+++ db.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Devconfig
+
+
+
+app = Flask(__name__)
+app.config.from_object(Devconfig)
+db = SQLAlchemy(app)
+
