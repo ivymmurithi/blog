56cdd5843bf97a47878241e3b31d565db0cc3c2b
ivymmurithi <ivymurithi@gmail.com>
2022-02-12 14:36:53 +0300

    finish sign up form logic, remove username from User class and add to html

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from db import  app, db
-from flask import render_template
+from flask import render_template,redirect,url_for
 from config import *
 from flask_script import Manager, Server
 from flask_migrate import Migrate,MigrateCommand
 from models.user_class import User
+from forms import SignupForm,LoginForm
 from werkzeug.security import check_password_hash,generate_password_hash
 from flask_login import LoginManager
 from flask_login import login_required
@@ -16,23 +17,49 @@ login_manager.session_protection = 'strong'
 login_manager.login_view = 'login'
 
 
-@app.route('/')
-def home():
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.before_first_request
+def create_user():
     db.create_all()
+
+
+@app.route('/',methods=['GET','POST'])
+def home():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login',methods=['GET','POST'])
 def login():
     return render_template('login.html')
 
 
-@app.route('/signup')
+@app.route('/signup',methods=['GET','POST'])
 def signup():
-    return render_template('signup.html')
+
+    signup_form = SignupForm()
+
+    if signup_form.validate_on_submit():
+        first_name = signup_form.first_name.data
+        last_name = signup_form.last_name.data
+        email = signup_form.email.data
+        password = signup_form.password.data
+
+        hashed_password = generate_password_hash(password, method="sha256")
+
+        new_user = User(first_name=first_name,last_name=last_name,email=email,password=hashed_password)
+
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('signup.html',signup_form=signup_form)
 
 
-@app.route('/posts')
+@app.route('/posts',methods=['GET','POST'])
 @login_required
 def posts():
     return render_template('posts.html')
--- models/user_class.py
+++ models/user_class.py
@@ -7,7 +7,6 @@ class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(255))
     last_name = db.Column(db.String(255))
-    username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True, index = True)
     password = db.Column(db.String(255))
 
