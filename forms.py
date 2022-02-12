001e7886220cb5286289459fb5545a2dba0651a4
ivymmurithi <ivymurithi@gmail.com>
2022-02-12 13:35:27 +0300

    add form fields and update User class

---
+++ forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField,PasswordField,EmailField,BooleanField
+from wtforms.validators import InputRequired,Email,Length
+
+class LoginForm(FlaskForm):
+    username = StringField(label='Username',validators=[InputRequired()])
+    email = EmailField(label='Your Email Address',validators=[InputRequired(),Email()])
+    password = PasswordField(label='Password',validators = [InputRequired(), Length(min=5, max = 50)])
+    remember = BooleanField(label='Remember me')
+    submit = SubmitField(label='Login')
+
+class SignupForm(FlaskForm):
+    first_name = StringField(label='First Name',validators=[InputRequired()])
+    last_name = StringField(label=' Last Name',validators=[InputRequired()])
+    username = StringField(label='Username',validators=[InputRequired()])
+    email = EmailField(label='Email',validators=[InputRequired(),Email(message = 'Invalid Email')])
+    password = PasswordField(label='Password',validators = [InputRequired(),Length(min=5, max=50)])
+    submit = SubmitField(label='Sign Up')
--- models/user_class.py
+++ models/user_class.py
@@ -3,9 +3,12 @@ from db import db
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer,primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
     username = db.Column(db.String(255))
-    password = db.Column(db.String)
+    email = db.Column(db.String(255),unique = True, index = True)
+    password = db.Column(db.String(255))
 
     def __repr__(self):
         return f'User{self.username}'
