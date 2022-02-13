2f923f0b518f88e4abc69f126269a1610eb1b856
ivymmurithi <ivymurithi@gmail.com>
2022-02-13 17:50:44 +0300

    add comments class in models

---
+++ models/comments.py
@@ -0,0 +1,15 @@
+from db import db
+from sqlalchemy.orm import relationship
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    post_id = db.Column(db.Integer,db.ForeignKey('posts.id'))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    user = relationship("User", backref="comments")
+
+
+    def __repr__(self):
+        return f'Comment {self.comment}'
