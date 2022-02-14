ae33aba2da535012b52837cf74b3fee2159d1f50
ivymmurithi <ivymurithi@gmail.com>
2022-02-14 15:25:30 +0300

    heroku deployment

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ class Config:
     FLASK_APP = 'main.py'
 
 class Prodconfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI=os.environ['SQLALCHEMY_DATABASE_URI']
+    SECRET_KEY=os.environ['SECRET_KEY']
 
 
 class Devconfig(Config):
     FLASK_ENV = 'development'
     SQLALCHEMY_DATABASE_URI=os.environ['SQLALCHEMY_DATABASE_URI']
     SECRET_KEY=os.environ['SECRET_KEY']
-    RANDOM_QUOTES_BASE_URL=os.environ['RANDOM_QUOTES_BASE_URL']
 
     DEBUG = True
--- db.py
+++ db.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import Devconfig
+from config import Devconfig, Prodconfig
 
 
 app = Flask(__name__)
-app.config.from_object(Devconfig)
+app.config.from_object(Prodconfig)
 db = SQLAlchemy(app)
 
